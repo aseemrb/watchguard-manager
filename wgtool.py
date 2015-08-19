b24763a35badecc18742af558026b12a1b2711c9
aseemraj <aseem.mathwiz@gmail.com>
2015-08-20 03:32:15 +0530

    Init commit

---
+++ wgtool.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+from wutility import *
+
+APPINDICATOR_ID = 'wgtool'
+AUTOMODE = True
+item_auto = None
+item_manual = None
+item_login = None
+item_logout = None
+
+def BuildMenu():
+    menu = gtk.Menu()
+    global item_auto, item_login, item_logout, item_manual
+    item_quit = gtk.MenuItem('Quit')
+    item_quit.connect('activate', Quit)
+    item_auto = gtk.MenuItem('✓Auto')
+    item_auto.connect('activate', WGAuto)
+    item_manual = gtk.MenuItem('Manual')
+    item_manual.connect('activate', WGManual)
+    item_login = gtk.MenuItem('Login')
+    item_login.connect('activate', WGLogin)
+    item_logout = gtk.MenuItem('Logout')
+    item_logout.connect('activate', WGLogout)
+    menu.append(item_auto)
+    menu.append(item_manual)
+    menu.append(gtk.SeparatorMenuItem())
+    menu.append(item_login)
+    menu.append(item_logout)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+def WGAuto(source):
+    LoopCheck()
+    gobject.timeout_add(AUTOTIMEOUT, LoopCheck)
+    item_auto.set_label('✓Auto')
+    item_manual.set_label('Manual')
+
+def LoopCheck():
+    global AUTOMODE
+    if InternetOn():
+        if not CheckStatus():
+            Login()
+    return AUTOMODE
+
+def WGManual(source):
+    global AUTOMODE
+    AUTOMODE = False
+    item_auto.set_label('Auto')
+    item_manual.set_label('✓Manual')
+
+
+def WGLogin(source):
+    if InternetOn():
+        isLoggedIn = CheckStatus()
+        if isLoggedIn:
+            SendNotification('You are already logged in', 'Logged In')
+        else:
+            Login()
+    else:
+        SendNotification('Internet down', 'Disconnected')
+
+def WGLogout(source):
+    if InternetOn():
+        isLoggedIn = CheckStatus()
+        if isLoggedIn:
+            Logout()
+        else:
+            SendNotification('Already logged out', 'Logged Out')
+            
+    else:
+        SendNotification('Internet down', 'Disconnected')
+
+def Quit(source):
+    notify.uninit()
+    gtk.main_quit()
+
+def main():
+    indicator = appindicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('icons/tray.svg'), appindicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(BuildMenu())
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    notify.init(APPINDICATOR_ID)
+    LoopCheck
+    gobject.timeout_add(AUTOTIMEOUT, LoopCheck)
+    gtk.main()
+
+if __name__ == "__main__":
+    main()
---
+++ wutility.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+import socket
+import signal
+import requests
+import httplib
+import os
+from urllib import urlencode
+from urllib2 import urlopen
+from os import system, devnull, path
+from sys import argv, exit
+from time import sleep
+from json import load
+from gi.repository import Gtk as gtk
+from gi.repository import AppIndicator3 as appindicator
+from gi.repository import Notify as notify
+from gi.repository import GObject as gobject
+from time import sleep
+
+# the base url where all requests will be made
+BASEURL = 'https://10.0.1.254:4100/wgcgi.cgi'
+
+# load the dictionary of credentials and the timeout for auto function
+DIRPATH = path.dirname(path.realpath(__file__))
+JSONDATA = load(open(DIRPATH + '/config.json'))
+CREDENTIALS = JSONDATA[0]
+AUTOTIMEOUT = JSONDATA[1]['timeout']*1000
+
+# login parameters
+PARAMSLOGIN = {
+    'fw_username': '',
+    'fw_password': '',
+    'fw_domain': 'gpra.in',
+    'submit': 'Login',
+    'action': 'fw_logon',
+    'fw_logon_type': 'logon',
+    'redirect': '',
+    'lang': 'en-US',
+}
+
+# logout parameters
+PARAMSLOGOUT = {
+    'Logout': 'Logout',
+    'action': 'fw_logon',
+    'fw_logon_type': 'logout'
+}
+
+# status check parameters
+PARAMSSTATUS = {
+    'action': 'fw_logon',
+    'fw_logon_type': 'status'
+}
+
+
+# login utility function
+def WatchguardLogin():
+    # iterate over available credentials and break when any of them succeeds
+    for username, password in CREDENTIALS.iteritems():
+        PARAMSLOGIN['fw_username'] = username
+        PARAMSLOGIN['fw_password'] = password
+        response = requests.post(BASEURL, data=PARAMSLOGIN, verify=False)
+        responseText = response.text
+
+        # return true and the username which was logged in successfully
+        if responseText.find('successfully authenticated') != -1:
+            return True, username
+
+    # return false if login was unsuccessful for all usernames
+    return False, username
+
+# logout utility function
+def WatchguardLogout():
+    try:
+        responseText = requests.post(BASEURL, data=PARAMSLOGOUT, verify=False)
+        return True
+    except:
+        return False
+
+# check login status, return true if logged in
+def CheckStatus():
+    response = requests.post('https://10.0.1.254:4100/wgcgi.cgi', data=PARAMSSTATUS, verify=False)
+    if response.text.find('successfully authenticated') != -1:
+        return True
+    return False
+
+# send the pretty system notification using notify-send
+def SendNotification(message, title='WatchGuard Tool'):
+    notify.Notification.new('<b>' + title + '</b>', message, DIRPATH + '/icons/tick.png').show()
+
+# login into watchguard
+def Login():
+    status, username = WatchguardLogin()
+    if status:
+        SendNotification('ID: ' + username, 'Logged In')
+    else:
+        SendNotification('Login failed. Recheck credentials', 'Failed')
+
+# logout from watchguard
+def Logout():
+    if WatchguardLogout():
+        SendNotification('Successfully logged out', 'Logged Out')
+    else:
+        SendNotification('Logout failed', 'Failed')
+
+# function to check if the internet connection is up
+def InternetOn():
+    try:
+        response = urlopen('http://google.com',timeout=2)
+        return True
+    except:
+        return False
